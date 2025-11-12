09dc66a24a6dbe032e5f514a1a440cdab615d9d8
mbgsudan <mo7medbabiker@gmail.com>
2025-11-12 17:12:56 +0200

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL = "https://qccqtwmywpsioexgdqwp.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlc3QiLCJpYXQiOjE2NjAwMDAwMDAsImV4cCI6MTk3NTU2MDAwMH0.abc123"; 
-const SUPABASE_BUCKET = "recordings";
